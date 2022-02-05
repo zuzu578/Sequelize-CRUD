d732f5971231b70b7a3f7ea9c346a2944477deb0
zuzu <dlwnghks6821@naver.com>
2022-02-05 14:12:16 +0900

    # 코드정리

--- sequelize/src/routes/crawlling.js
+++ sequelize/src/routes/crawlling.js
@@ -9,26 +9,18 @@ let genre = req.param("genre");
 if(!genre){
   genre = "game"
 }
-console.log("genre ===> " , genre);
-const getHtml = async () => {
-    try {
-      return await axios.get(`https://taiko.namco-ch.net/taiko/songlist/${genre}.php`);
-    } catch (error) {
-      console.error(error);
-    }
-  };
-  getHtml()
-    .then(html => {
-      let songList = [];
-      const $ = cheerio.load(html.data);
-      const $bodyList = $("table").children("tbody").toArray();
-      $bodyList.forEach((i)=>{
-        songList.push($(i).find('tr').text().replace(/(\s*)/g, "").split("-"))
-      });
-      console.log(songList[1]);
-      res.send(songList[1]);
-    })
-   
+try{
+  const getHtml = await axios.get(`https://taiko.namco-ch.net/taiko/songlist/${genre}.php`);
+  let songList = [];
+    const $ = cheerio.load(getHtml.data);
+    const $bodyList = $("table").children("tbody").toArray();
+    $bodyList.forEach((i)=>{
+      songList.push($(i).find('tr').text().replace(/(\s*)/g, "").split("-"))
+    });
+    res.send(songList[1]);
+}catch(error){
+  console.log(error.message);
+}
 });
 
 module.exports = router;
